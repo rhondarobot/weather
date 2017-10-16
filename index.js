36536b1830c24dbc26c55651121276bda4eaa94a
rhondarobot <rrachellej@gmail.com>
2017-10-16 13:01:38 -0700

    weather added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 * @param {Object} req Cloud Function request context.
 * @param {Object} res Cloud Function response context.
 */
-exports.helloHttp = function helloHttp (req, res) {
+exports.weather = function weather (req, res) {
   response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
   
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
